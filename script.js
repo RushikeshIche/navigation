14c9d3499277c5be677a81ff8d77fd85c32cb60f
Rushikesh Iche <138008769+RushikeshIche@users.noreply.github.com>
2024-03-05 18:01:47 +0530

    Updated script.js for maps directory

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ playground.addEventListener('click', ()=>{
     let map = document.getElementsByClassName('map')[0];
     map.src = "maps/playground/playground.html";
 })
+let cseandece = document.getElementsByClassName('cseandece')[0];
+cseandece.addEventListener('click', ()=>{
+    console.log("Clicked")
+    let map = document.getElementsByClassName('map')[0];
+    map.src = "maps/cseandece/cseandece.html";
+})
+let mech = document.getElementsByClassName('mech')[0];
+mech.addEventListener('click', ()=>{
+    console.log("Clicked")
+    let map = document.getElementsByClassName('map')[0];
+    map.src = "maps/mech/mech.html";
+})
 let delhi = document.getElementsByClassName('delhi')[0];
 delhi.addEventListener('click', ()=>{
     console.log("Clicked")
