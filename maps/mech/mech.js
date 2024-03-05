e112f9658a3bac4f345693664275faba544ca7fb
Rushikesh Iche <138008769+RushikeshIche@users.noreply.github.com>
2024-03-05 17:58:55 +0530

    Updates maps Directory

---
+++ maps/mech/mech.js
@@ -0,0 +1,7 @@
+let visitBtn0=document.getElementsByClassName('visitBtn0')[0];
+
+visitBtn.addEventListener('click', ()=>{
+    console.log("Clicked")
+    
+    location = "cseandece0/cseandece0.html";
+})
