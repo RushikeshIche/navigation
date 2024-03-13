fb6527b7d59994c7ee4052228c8ec06313f6ea4f
Rushikesh Iche <138008769+RushikeshIche@users.noreply.github.com>
2024-03-13 09:35:59 +0530

    Delete maps directory to update again

--- maps/mech/mech.js
+++
@@ -1,7 +0,0 @@
-let visitBtn0=document.getElementsByClassName('visitBtn0')[0];
-
-visitBtn.addEventListener('click', ()=>{
-    console.log("Clicked")
-    
-    location = "cseandece0/cseandece0.html";
-})
