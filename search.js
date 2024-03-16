820d9fcc55f07b73692648e18a67ed711e9836c5
arnav7897 <146191610+arnav7897@users.noreply.github.com>
2024-03-16 21:20:26 +0530

    Update search.js
    ADDED JS FOR WORKING OF SEARCH OPTION

--- search.js
+++ search.js
@@ -1,56 +1,20 @@
-const spaces = {
-    playground: [
-        "sardar vallabhbhai patel indoor stadium",
-        "VolleyBall Court - 1",
-        "VolleyBall Court - 2",
-        "BasketBall Court",
-        "Cricket Practice Nets",
-        "Tennis Court",
-        "Kabaddi Court",
-        "Signal Tower",
-        ],
-    cseandece0 :[
-        "CSE ECE Ground Floor Hall",
-        "Reading Hall",
-        "Fire Pump Station",
-        "Microprocessor and Microcontroller Lab",
-        "Electrical Devices and Instrumental Lab",
-        "Drone Lab",
-        
-
-    ],
-    cseandece1:[
-
-    ],
-    csesandece2:
-    [
-
-    ],
-
-
+=
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
-console.log(spaces[playground])
-/* for(let i=0;i<items.length;i++){
-    var html= `<tr class="tr">
-                    
-                    <td class="item_name">${items[i]}</td>
-                </tr>`;
-       let a =document.getElementById("food")
-       a.innerHTML = a.innerHTML + html;
-     }
-function arnav() {
-    var input, filter, tr, a, i, txtValue;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    tr = document.getElementsByClassName('item_name');
-    for (i = 0; i < tr.length; i++) {
-      a = tr[i];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        document.getElementsByClassName("tr")[i].style.display = "";
-      } else {
-        document.getElementsByClassName("tr")[i].style.display = "none";
-      }
+function arnav (){
+  var ui, f, ul, li, a, i;
+  ui = document.getElementById("myInput");
+  f = ui.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(f) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
     }
-  } */
+  }
+}
