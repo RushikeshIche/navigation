206762d89d99cb76cdafa910c8f379bce746adfb
arnav7897 <146191610+arnav7897@users.noreply.github.com>
2024-03-17 11:49:19 +0530

    Update iframecontent.js
    added search bar functionality in order to embend all searches into iframe

--- iframecontent.js
+++ iframecontent.js
@@ -29,6 +29,86 @@ delhi.addEventListener('click', ()=>{
     let map = document.getElementsByClassName('map')[0];
     map.src = "maps/delhi/delhi.html";
 })
+// search option js code for main search options to embend in map
+
+for (i = 0; i < 9; i++) {
+    var cseandece2 = document.getElementsByClassName('arnav_search')[`${i}`]
+    cseandece2.addEventListener('click', () => {
+        console.log("Clicked")
+        let map = document.getElementsByClassName('map')[0];
+        map.src = "maps/cseandece/cseandece2/cseandece2.html";
+    })
+}
+for (i = 9; i < 16; i++) {
+    var academic1 = document.getElementsByClassName('arnav_search')[`${i}`]
+    academic1.addEventListener('click', () => {
+        console.log("Clicked")
+        let map = document.getElementsByClassName('map')[0];
+        map.src = "maps/new-academic/new-academic1/new-academic1.html";
+    })
+}
+for (i = 16; i < 22; i++) {
+    var cseandece1 = document.getElementsByClassName('arnav_search')[`${i}`]
+    cseandece1.addEventListener('click', () => {
+        console.log("Clicked")
+        let map = document.getElementsByClassName('map')[0];
+        map.src = "maps/cseandece/cseandece1/cseandece1.html";
+    })
+}
+for (i = 22; i < 29; i++) {
+    var playground1 = document.getElementsByClassName('arnav_search')[`${i}`]
+    playground1.addEventListener('click', () => {
+        console.log("Clicked")
+        let map = document.getElementsByClassName('map')[0];
+        map.src = "maps/playground/playground.html";
+    })
+}
+for (i = 29; i < 38; i++) {
+    var academic0 = document.getElementsByClassName('arnav_search')[`${i}`]
+    academic0.addEventListener('click', () => {
+        console.log("Clicked")
+        let map = document.getElementsByClassName('map')[0];
+        map.src = "maps/new-academic/new-academic0/new-academic0.html";
+    })
+}
+
+for (i = 29; i < 38; i++) {
+    var academic0 = document.getElementsByClassName('arnav_search')[`${i}`]
+    academic0.addEventListener('click', () => {
+        console.log("Clicked")
+        let map = document.getElementsByClassName('map')[0];
+        map.src = "maps/new-academic/new-academic0/new-academic0.html";
+    })
+}
+
+for (i = 38; i < 43; i++) {
+    var mech1 = document.getElementsByClassName('arnav_search')[`${i}`]
+    mech1.addEventListener('click', () => {
+        console.log("Clicked")
+        let map = document.getElementsByClassName('map')[0];
+        map.src = "maps/mech/mech1/mech1.html";
+    })
+}
+
+for (i = 43; i < 45; i++) {
+    var mech0 = document.getElementsByClassName('arnav_search')[`${i}`]
+    mech0.addEventListener('click', () => {
+        console.log("Clicked")
+        let map = document.getElementsByClassName('map')[0];
+        map.src = "maps/mech/mech0/mech0.html";
+    })
+}
+
+for (i = 45; i < 56; i++) {
+    var cseandece0 = document.getElementsByClassName('arnav_search')[`${i}`]
+    cseandece0.addEventListener('click', () => {
+        console.log("Clicked")
+        let map = document.getElementsByClassName('map')[0];
+        map.src = "maps/cseandece/cseandece0/cseandece0.html";
+    })
+}
+
+// codes of embending in map ends here
 let c = 45;
 
 function draw(){
