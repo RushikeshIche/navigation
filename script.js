60ab1b4c0d3afc2e032c4168ae60cf2341ec51a4
RushikeshIche <rushikeshiche7@gmail.com>
2024-03-17 17:04:48 +0530

    minor bug fixes again

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
+let campus = document.getElementsByClassName('campus')[0];
+campus.addEventListener('click', ()=>{
+    console.log("Clicked")
+    let map = document.getElementsByClassName('map')[0];
+    map.src = "maps/campus/campus.html";
+})
+
 let playground = document.getElementsByClassName('playground')[0];
 playground.addEventListener('click', ()=>{
     console.log("Clicked")
@@ -16,6 +24,12 @@ mech.addEventListener('click', ()=>{
     let map = document.getElementsByClassName('map')[0];
     map.src = "maps/mech/mech.html";
 })
+let srkh = document.getElementsByClassName('srkh')[0];
+srkh.addEventListener('click', ()=>{
+    console.log("Clicked")
+    let map = document.getElementsByClassName('map')[0];
+    map.src = "maps/srkh/srkh.html";
+})
 let delhi = document.getElementsByClassName('delhi')[0];
 delhi.addEventListener('click', ()=>{
     console.log("Clicked")
@@ -26,7 +40,13 @@ let newacademic = document.getElementsByClassName('new-academic')[0];
 newacademic.addEventListener('click', ()=>{
     console.log("Clicked")
     let map = document.getElementsByClassName('map')[0];
-    map.src = "maps/new-academic/newacademic.html";
+    map.src = "maps/new-academic/new-academic.html";
+})
+let oldacademic = document.getElementsByClassName('old-academic')[0];
+oldacademic.addEventListener('click', ()=>{
+    console.log("Clicked")
+    let map = document.getElementsByClassName('map')[0];
+    map.src = "maps/old-academic/old-academic.html";
 })
 let mvhr = document.getElementsByClassName('mvhr')[0];
 mvhr.addEventListener('click', ()=>{
@@ -34,11 +54,11 @@ mvhr.addEventListener('click', ()=>{
     let map = document.getElementsByClassName('map')[0];
     map.src = "maps/mvhr/mvhr.html";
 })
-let kchor = document.getElementsByClassName('kalpana-chawla')[0];
+let kchor = document.getElementsByClassName('kchor')[0];
 kchor.addEventListener('click', ()=>{
     console.log("Clicked")
     let map = document.getElementsByClassName('map')[0];
-    map.src = "maps/kalpana-chawla/kalpana-chawla.html";
+    map.src = "maps/kchor/kchor.html";
 })
 
 let c = 45;
