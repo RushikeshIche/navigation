2c5ccbfc22f1d63ca557786262bdee9d375904c5
arnav7897 <146191610+arnav7897@users.noreply.github.com>
2024-03-18 14:20:51 +0530

    Update iframecontent.js
    added js to make it more functional

--- iframecontent.js
+++ iframecontent.js
@@ -1,4 +1,7 @@
 
+
+// adding classes 
+
 // search option js code for main search options to embend in map
 
 for (i = 0; i < 9; i++) {
@@ -6,7 +9,13 @@ for (i = 0; i < 9; i++) {
     cseandece2.addEventListener('click', () => {
         console.log("Clicked")
         let map = document.getElementsByClassName('map')[0];
-        map.src = "maps/cseandece/cseandece2/cseandece2.html";
+        map.src = "maps/cseandece/cseandece3/cseandece3.html";
+        let display =document.getElementById("searchdisplay");
+        display.style.display="";
+        display.innerHTML="<h1>THIS IS CSE AND ECE FLOOR 3</h1>";
+        let anchor =document.getElementById("asearch");
+        anchor.style.display="";
+        anchor.href="https://www.google.com/maps/place/15%C2%B045'40.6%22N+78%C2%B002'18.0%22E/@15.761278,78.0376973,279m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d15.761278!4d78.038341?entry=ttu";
     })
 }
 for (i = 9; i < 16; i++) {
@@ -15,6 +24,12 @@ for (i = 9; i < 16; i++) {
         console.log("Clicked")
         let map = document.getElementsByClassName('map')[0];
         map.src = "maps/new-academic/new-academic1/new-academic1.html";
+        let display =document.getElementById("searchdisplay");
+        display.style.display="";
+        display.innerHTML="<h1>THIS IS NEW-ACADEMIC FLOOR 1</h1>";
+        let anchor =document.getElementById("asearch");
+        anchor.style.display="";
+        anchor.href="https://www.google.com/maps/place/15%C2%B045'40.9%22N+78%C2%B002'20.9%22E/@15.761361,78.0384853,279m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d15.761361!4d78.039129?entry=ttu";
     })
 }
 for (i = 16; i < 22; i++) {
@@ -22,7 +37,13 @@ for (i = 16; i < 22; i++) {
     cseandece1.addEventListener('click', () => {
         console.log("Clicked")
         let map = document.getElementsByClassName('map')[0];
-        map.src = "maps/cseandece/cseandece1/cseandece1.html";
+        map.src = "maps/cseandece/cseandece2/cseandece2.html";
+        let display =document.getElementById("searchdisplay");
+        display.style.display="";
+        display.innerHTML="<h1>THIS IS CSE AND ECE FLOOR 2</h1>";
+        let anchor =document.getElementById("asearch");
+        anchor.style.display="";
+        anchor.href="https://www.google.com/maps/place/15%C2%B045'40.6%22N+78%C2%B002'18.0%22E/@15.761278,78.0376973,279m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d15.761278!4d78.038341?entry=ttu";
     })
 }
 for (i = 22; i < 29; i++) {
@@ -31,6 +52,12 @@ for (i = 22; i < 29; i++) {
         console.log("Clicked")
         let map = document.getElementsByClassName('map')[0];
         map.src = "maps/playground/playground.html";
+        let display =document.getElementById("searchdisplay");
+        display.style.display="";
+        display.innerHTML="<h1>THIS IS SPORTS COMPLEX</h1>";
+        let anchor =document.getElementById("asearch");
+        anchor.style.display="";
+        anchor.href="https://www.google.com/maps/place/Indoor+Stadium-IIITDMK/@15.7590015,78.0342926,1116m/data=!3m1!1e3!4m6!3m5!1s0x3bb5ddae23a961d3:0xfb836b7c509f72d3!8m2!3d15.7590012!4d78.0356758!16s%2Fg%2F11tshcg867?entry=ttu";
     })
 }
 for (i = 29; i < 38; i++) {
@@ -39,15 +66,12 @@ for (i = 29; i < 38; i++) {
         console.log("Clicked")
         let map = document.getElementsByClassName('map')[0];
         map.src = "maps/new-academic/new-academic0/new-academic0.html";
-    })
-}
-
-for (i = 29; i < 38; i++) {
-    var academic0 = document.getElementsByClassName('arnav_search')[`${i}`]
-    academic0.addEventListener('click', () => {
-        console.log("Clicked")
-        let map = document.getElementsByClassName('map')[0];
-        map.src = "maps/new-academic/new-academic0/new-academic0.html";
+        let display =document.getElementById("searchdisplay");
+        display.style.display="";
+        display.innerHTML="<h1>THIS IS NEW ACADEMICS FLOOR 0</h1>";
+        let anchor =document.getElementById("asearch");
+        anchor.style.display="";
+        anchor.href="https://www.google.com/maps/place/15%C2%B045'40.9%22N+78%C2%B002'20.9%22E/@15.761361,78.0384853,279m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d15.761361!4d78.039129?entry=ttu";
     })
 }
 
@@ -57,6 +81,13 @@ for (i = 38; i < 43; i++) {
         console.log("Clicked")
         let map = document.getElementsByClassName('map')[0];
         map.src = "maps/mech/mech1/mech1.html";
+        let display =document.getElementById("searchdisplay");
+        display.style.display="";
+        display.innerHTML="<h1>THIS IS MECHANICAL BUILDING FLOOR 1</h1>";
+        let anchor =document.getElementById("asearch");
+        anchor.style.display="";
+        anchor.href="https://www.google.com/maps/place/IIITDM+KURNOOL+CENTRAL+WORKSHOP/@15.7597796,78.0399145,556m/data=!3m1!1e3!4m6!3m5!1s0x3bb5dd97f31e49b1:0x516d16aa978028d2!8m2!3d15.760187!4d78.0397524!16s%2Fg%2F11sbtpwr3g?entry=ttu";
+        
     })
 }
 
@@ -66,6 +97,12 @@ for (i = 43; i < 45; i++) {
         console.log("Clicked")
         let map = document.getElementsByClassName('map')[0];
         map.src = "maps/mech/mech0/mech0.html";
+        let display =document.getElementById("searchdisplay");
+        display.style.display="";
+        display.innerHTML="<h1>THIS IS MECHANICAL BUILDING FLOOR 0</h1>";
+        let anchor =document.getElementById("asearch");
+        anchor.style.display="";
+        anchor.href="https://www.google.com/maps/place/IIITDM+KURNOOL+CENTRAL+WORKSHOP/@15.7597796,78.0399145,556m/data=!3m1!1e3!4m6!3m5!1s0x3bb5dd97f31e49b1:0x516d16aa978028d2!8m2!3d15.760187!4d78.0397524!16s%2Fg%2F11sbtpwr3g?entry=ttu";
     })
 }
 
@@ -75,6 +112,12 @@ for (i = 45; i < 56; i++) {
         console.log("Clicked")
         let map = document.getElementsByClassName('map')[0];
         map.src = "maps/cseandece/cseandece0/cseandece0.html";
+        let display =document.getElementById("searchdisplay");
+        display.style.display="";
+        display.innerHTML="<h1>THIS IS CSE AND ECE BUILDING FLOOR 0</h1>";
+        let anchor =document.getElementById("asearch");
+        anchor.style.display="";
+        anchor.href="https://www.google.com/maps/place/15%C2%B045'40.6%22N+78%C2%B002'18.0%22E/@15.761278,78.0376973,279m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d15.761278!4d78.038341?entry=ttu";
     })
 }
 for (i=56;i<66;i++){
@@ -83,6 +126,12 @@ for (i=56;i<66;i++){
         console.log("Clicked")
         let map = document.getElementsByClassName('map')[0];
         map.src = "maps/old-academic/old-academic0/old-academic0.html";
+        let display =document.getElementById("searchdisplay");
+        display.style.display="";
+        display.innerHTML="<h1>THIS IS OLD ACADEMIC BUILDING FLOOR 0</h1>";
+        let anchor =document.getElementById("asearch");
+        anchor.style.display="";
+        anchor.href="https://www.google.com/maps/place/Indian+Institute+Of+Information+Technology,+Design+%26+Manufacturing,+Kurnool/@15.756247,78.0434222,2734m/data=!3m1!1e3!4m6!3m5!1s0x3bb5dc3bfcf99645:0x52358ddcfb659cb9!8m2!3d15.7617232!4d78.0363767!16s%2Fg%2F11b8cdwgnh?entry=ttu";
     })
 }
     for (i=66;i<73;i++){
@@ -91,6 +140,13 @@ for (i=56;i<66;i++){
             console.log("Clicked")
             let map = document.getElementsByClassName('map')[0];
             map.src = "maps/old-academic/old-academic1/old-academic1.html";
+            let display =document.getElementById("searchdisplay");
+            display.style.display="";
+            display.innerHTML="<h1>THIS IS OLD ACADEMIC BUILDING FLOOR 1</h1>";
+        
+        let anchor =document.getElementById("asearch");
+        anchor.style.display="";
+        anchor.href="https://www.google.com/maps/place/Indian+Institute+Of+Information+Technology,+Design+%26+Manufacturing,+Kurnool/@15.756247,78.0434222,2734m/data=!3m1!1e3!4m6!3m5!1s0x3bb5dc3bfcf99645:0x52358ddcfb659cb9!8m2!3d15.7617232!4d78.0363767!16s%2Fg%2F11b8cdwgnh?entry=ttu";
         })
 }
 // codes of embending in map ends here
