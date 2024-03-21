f77986a0ee9bebc4d05573c6b5e23d133adc8d56
arnav7897 <146191610+arnav7897@users.noreply.github.com>
2024-03-21 23:31:01 +0530

    Update iframecontent.js
    added more elements

--- iframecontent.js
+++ iframecontent.js
@@ -5,8 +5,8 @@
 // search option js code for main search options to embend in map
 
 for (i = 0; i < 9; i++) {
-    var cseandece2 = document.getElementsByClassName('arnav_search')[`${i}`]
-    cseandece2.addEventListener('click', () => {
+    var cseandece3 = document.getElementsByClassName('arnav_search')[`${i}`]
+    cseandece3.addEventListener('click', () => {
         console.log("Clicked")
         let map = document.getElementsByClassName('map')[0];
         map.src = "maps/cseandece/cseandece3/cseandece3.html";
@@ -15,10 +15,11 @@ for (i = 0; i < 9; i++) {
         display.innerHTML="<h1>THIS IS CSE AND ECE FLOOR 3</h1>";
         let anchor =document.getElementById("asearch");
         anchor.style.display="";
+        anchor.innerHTML="<H3>CLICK HERE FOR DIRECTIONS</H3>"
         anchor.href="https://www.google.com/maps/place/15%C2%B045'40.6%22N+78%C2%B002'18.0%22E/@15.761278,78.0376973,279m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d15.761278!4d78.038341?entry=ttu";
     })
 }
-for (i = 9; i < 16; i++) {
+for (i = 9; i < 18; i++) {
     var academic1 = document.getElementsByClassName('arnav_search')[`${i}`]
     academic1.addEventListener('click', () => {
         console.log("Clicked")
@@ -29,12 +30,13 @@ for (i = 9; i < 16; i++) {
         display.innerHTML="<h1>THIS IS NEW-ACADEMIC FLOOR 1</h1>";
         let anchor =document.getElementById("asearch");
         anchor.style.display="";
+        anchor.innerHTML="<H3>CLICK HERE FOR DIRECTIONS</H3>"
         anchor.href="https://www.google.com/maps/place/15%C2%B045'40.9%22N+78%C2%B002'20.9%22E/@15.761361,78.0384853,279m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d15.761361!4d78.039129?entry=ttu";
     })
 }
-for (i = 16; i < 23; i++) {
-    var cseandece1 = document.getElementsByClassName('arnav_search')[`${i}`]
-    cseandece1.addEventListener('click', () => {
+for (i = 18; i < 25; i++) {
+    var cseandece2 = document.getElementsByClassName('arnav_search')[`${i}`]
+    cseandece2.addEventListener('click', () => {
         console.log("Clicked")
         let map = document.getElementsByClassName('map')[0];
         map.src = "maps/cseandece/cseandece2/cseandece2.html";
@@ -43,10 +45,11 @@ for (i = 16; i < 23; i++) {
         display.innerHTML="<h1>THIS IS CSE AND ECE FLOOR 2</h1>";
         let anchor =document.getElementById("asearch");
         anchor.style.display="";
+        anchor.innerHTML="<H3>CLICK HERE FOR DIRECTIONS</H3>"
         anchor.href="https://www.google.com/maps/place/15%C2%B045'40.6%22N+78%C2%B002'18.0%22E/@15.761278,78.0376973,279m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d15.761278!4d78.038341?entry=ttu";
     })
 }
-for (i = 23; i < 30; i++) {
+for (i = 25; i < 35; i++) {
     var playground1 = document.getElementsByClassName('arnav_search')[`${i}`]
     playground1.addEventListener('click', () => {
         console.log("Clicked")
@@ -57,10 +60,11 @@ for (i = 23; i < 30; i++) {
         display.innerHTML="<h1>THIS IS SPORTS COMPLEX</h1>";
         let anchor =document.getElementById("asearch");
         anchor.style.display="";
+        anchor.innerHTML="<H3>CLICK HERE FOR DIRECTIONS</H3>"
         anchor.href="https://www.google.com/maps/place/Indoor+Stadium-IIITDMK/@15.7590015,78.0342926,1116m/data=!3m1!1e3!4m6!3m5!1s0x3bb5ddae23a961d3:0xfb836b7c509f72d3!8m2!3d15.7590012!4d78.0356758!16s%2Fg%2F11tshcg867?entry=ttu";
     })
 }
-for (i = 30; i < 39; i++) {
+for (i = 35; i < 44; i++) {
     var academic0 = document.getElementsByClassName('arnav_search')[`${i}`]
     academic0.addEventListener('click', () => {
         console.log("Clicked")
@@ -71,11 +75,12 @@ for (i = 30; i < 39; i++) {
         display.innerHTML="<h1>THIS IS NEW ACADEMICS FLOOR 0</h1>";
         let anchor =document.getElementById("asearch");
         anchor.style.display="";
+        anchor.innerHTML="<H3>CLICK HERE FOR DIRECTIONS</H3>"
         anchor.href="https://www.google.com/maps/place/15%C2%B045'40.9%22N+78%C2%B002'20.9%22E/@15.761361,78.0384853,279m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d15.761361!4d78.039129?entry=ttu";
     })
 }
 
-for (i = 39; i < 44; i++) {
+for (i = 44; i < 49; i++) {
     var mech1 = document.getElementsByClassName('arnav_search')[`${i}`]
     mech1.addEventListener('click', () => {
         console.log("Clicked")
@@ -86,12 +91,13 @@ for (i = 39; i < 44; i++) {
         display.innerHTML="<h1>THIS IS MECHANICAL BUILDING FLOOR 1</h1>";
         let anchor =document.getElementById("asearch");
         anchor.style.display="";
+        anchor.innerHTML="<H3>CLICK HERE FOR DIRECTIONS</H3>"
         anchor.href="https://www.google.com/maps/place/IIITDM+KURNOOL+CENTRAL+WORKSHOP/@15.7597796,78.0399145,556m/data=!3m1!1e3!4m6!3m5!1s0x3bb5dd97f31e49b1:0x516d16aa978028d2!8m2!3d15.760187!4d78.0397524!16s%2Fg%2F11sbtpwr3g?entry=ttu";
         
     })
 }
 
-for (i = 44; i < 45; i++) {
+for (i = 49; i < 51; i++) {
     var mech0 = document.getElementsByClassName('arnav_search')[`${i}`]
     mech0.addEventListener('click', () => {
         console.log("Clicked")
@@ -102,11 +108,12 @@ for (i = 44; i < 45; i++) {
         display.innerHTML="<h1>THIS IS MECHANICAL BUILDING FLOOR 0</h1>";
         let anchor =document.getElementById("asearch");
         anchor.style.display="";
+        anchor.innerHTML="<H3>CLICK HERE FOR DIRECTIONS</H3>"
         anchor.href="https://www.google.com/maps/place/IIITDM+KURNOOL+CENTRAL+WORKSHOP/@15.7597796,78.0399145,556m/data=!3m1!1e3!4m6!3m5!1s0x3bb5dd97f31e49b1:0x516d16aa978028d2!8m2!3d15.760187!4d78.0397524!16s%2Fg%2F11sbtpwr3g?entry=ttu";
     })
 }
 
-for (i = 45; i < 57; i++) {
+for (i = 51; i < 62; i++) {
     var cseandece0 = document.getElementsByClassName('arnav_search')[`${i}`]
     cseandece0.addEventListener('click', () => {
         console.log("Clicked")
@@ -117,10 +124,11 @@ for (i = 45; i < 57; i++) {
         display.innerHTML="<h1>THIS IS CSE AND ECE BUILDING FLOOR 0</h1>";
         let anchor =document.getElementById("asearch");
         anchor.style.display="";
+        anchor.innerHTML="<H3>CLICK HERE FOR DIRECTIONS</H3>"
         anchor.href="https://www.google.com/maps/place/15%C2%B045'40.6%22N+78%C2%B002'18.0%22E/@15.761278,78.0376973,279m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d15.761278!4d78.038341?entry=ttu";
     })
 }
-for (i=57;i<67;i++){
+for (i=62;i<72;i++){
     var oldacademic0 = document.getElementsByClassName('arnav_search')[i]
     oldacademic0.addEventListener('click', () => {
         console.log("Clicked")
@@ -131,11 +139,12 @@ for (i=57;i<67;i++){
         display.innerHTML="<h1>THIS IS OLD ACADEMIC BUILDING FLOOR 0</h1>";
         let anchor =document.getElementById("asearch");
         anchor.style.display="";
+        anchor.innerHTML="<H3>CLICK HERE FOR DIRECTIONS</H3>"
         anchor.href="https://www.google.com/maps/place/Indian+Institute+Of+Information+Technology,+Design+%26+Manufacturing,+Kurnool/@15.756247,78.0434222,2734m/data=!3m1!1e3!4m6!3m5!1s0x3bb5dc3bfcf99645:0x52358ddcfb659cb9!8m2!3d15.7617232!4d78.0363767!16s%2Fg%2F11b8cdwgnh?entry=ttu";
         document.getElementsByClassName("oldacad")[i-56].style.color="yellow";
     })
 }
-    for (i=67;i<74;i++){
+    for (i=72;i<79;i++){
         var oldacademic1 = document.getElementsByClassName('arnav_search')[`${i}`]
         oldacademic1.addEventListener('click', () => {
             console.log("Clicked")
@@ -147,8 +156,59 @@ for (i=57;i<67;i++){
         
         let anchor =document.getElementById("asearch");
         anchor.style.display="";
+        anchor.innerHTML="<H3>CLICK HERE FOR DIRECTIONS</H3>"
         anchor.href="https://www.google.com/maps/place/Indian+Institute+Of+Information+Technology,+Design+%26+Manufacturing,+Kurnool/@15.756247,78.0434222,2734m/data=!3m1!1e3!4m6!3m5!1s0x3bb5dc3bfcf99645:0x52358ddcfb659cb9!8m2!3d15.7617232!4d78.0363767!16s%2Fg%2F11b8cdwgnh?entry=ttu";
         })
+}
+for(i=79;i<83;i++){
+    var cseandece1 = document.getElementsByClassName('arnav_search')[`${i}`]
+    cseandece1.addEventListener('click', () => {
+        console.log("Clicked")
+        let map = document.getElementsByClassName('map')[0];
+        map.src = "maps/cseandece/cseandece3/cseandece3.html";
+        let display =document.getElementById("searchdisplay");
+        display.style.display="";
+        display.innerHTML="<h1>THIS IS CSE AND ECE FLOOR 3</h1>";
+        let anchor =document.getElementById("asearch");
+        anchor.style.display="";
+        anchor.innerHTML="<H3>CLICK HERE FOR DIRECTIONS</H3>"
+        anchor.href="https://www.google.com/maps/place/15%C2%B045'40.6%22N+78%C2%B002'18.0%22E/@15.761278,78.0376973,279m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d15.761278!4d78.038341?entry=ttu";
+    })
+}
+for(i=83;i<84;i++){
+let mvhr = document.getElementsByClassName('arnav_search')[i];
+mvhr.addEventListener('click', ()=>{
+    console.log("Clicked")
+    let map = document.getElementsByClassName('map')[0];
+    map.src = "maps/mvhr/mvhr.html";
+})
+}
+
+for(i=84;i<85;i++){
+let srkh = document.getElementsByClassName('arnav_search')[i];
+srkh.addEventListener('click', ()=>{
+    console.log("Clicked")
+    let map = document.getElementsByClassName('map')[0];
+    map.src = "maps/srkh/srkh.html";
+
+})  
+}
+
+for(i=85;i<86;i++){
+let kalpana = document.getElementsByClassName('arnav_search')[i];
+kalpna.addEventListener('click', ()=>{
+    console.log("Clicked")
+    let map = document.getElementsByClassName('map')[0];
+    map.src = "maps/kchor/kchor.html";
+}) }
+
+for(i=86;i<87;i++){ 
+let delhi = document.getElementsByClassName('arnav_search')[i];
+delhi.addEventListener('click', ()=>{
+    console.log("Clicked")
+    let map = document.getElementsByClassName('map')[0];
+    map.src = "maps/delhi/delhi.html";
+})  
 }
 // codes of embending in map ends here
 
