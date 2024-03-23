1792ae16a4f1d85b2bfe117c859269df35446919
arnav7897 <146191610+arnav7897@users.noreply.github.com>
2024-03-24 00:18:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,163 +1,160 @@
 
 let campus = document.getElementsByClassName('campus')[0];
-campus.addEventListener('click', ()=>{
+campus.addEventListener('click', () => {
     console.log("Clicked")
     let map = document.getElementsByClassName('map')[0];
     map.src = "maps/campus/campus.html";
-    let display =document.getElementById("searchdisplay");
-    display.style.display="none";
-    let anchor =document.getElementById("asearch");
-    anchor.style.display="none";
+    let display = document.getElementById("searchdisplay");
+    display.style.display = "";
+    display.innerHTML = "<h1>THIS IS MECHANICAL BUILDING</h1>";
+    let anchor = document.getElementById("asearch");
+    anchor.style.display = "none";
 })
 
 let playground = document.getElementsByClassName('playground')[0];
-playground.addEventListener('click', ()=>{
+playground.addEventListener('click', () => {
     console.log("Clicked")
     let map = document.getElementsByClassName('map')[0];
     map.src = "maps/playground/playground.html";
-    let display =document.getElementById("searchdisplay");
-        display.style.display="";
-        display.innerHTML="<h1>THIS IS SPORTS COMPLEX</h1>";
-        let anchor =document.getElementById("asearch");
-        anchor.style.display="";
-        anchor.innerHTML="<H3>CLICK HERE FOR DIRECTIONS</H3>"
-        anchor.href="https://www.google.com/maps/place/Indoor+Stadium-IIITDMK/@15.7590015,78.0342926,1116m/data=!3m1!1e3!4m6!3m5!1s0x3bb5ddae23a961d3:0xfb836b7c509f72d3!8m2!3d15.7590012!4d78.0356758!16s%2Fg%2F11tshcg867?entry=ttu";
+    let display = document.getElementById("searchdisplay");
+    display.style.display = "";
+    display.innerHTML = "<h1>THIS IS SPORTS COMPLEX</h1>";
+    let anchor = document.getElementById("asearch");
+    anchor.style.display = "";
+    anchor.innerHTML = "<H3>CLICK HERE FOR DIRECTIONS</H3>"
+    anchor.href = "https://www.google.com/maps/place/Indoor+Stadium-IIITDMK/@15.7590015,78.0342926,1116m/data=!3m1!1e3!4m6!3m5!1s0x3bb5ddae23a961d3:0xfb836b7c509f72d3!8m2!3d15.7590012!4d78.0356758!16s%2Fg%2F11tshcg867?entry=ttu";
 })
 
-})
 let cseandece = document.getElementsByClassName('cseandece')[0];
-cseandece.addEventListener('click', ()=>{
+cseandece.addEventListener('click', () => {
     console.log("Clicked")
     let map = document.getElementsByClassName('map')[0];
     map.src = "maps/cseandece/cseandece.html";
-   et display =document.getElementById("searchdisplay");
-        display.style.display="";
-        display.innerHTML="<h1>THIS IS CSE AND ECE BUILDINGS</h1>";
-        let anchor =document.getElementById("asearch");
-        anchor.style.display="";
-        anchor.innerHTML="<H3>CLICK HERE FOR DIRECTIONS</H3>"
-        anchor.href="https://www.google.com/maps/place/15%C2%B045'40.6%22N+78%C2%B002'18.0%22E/@15.761278,78.0376973,279m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d15.761278!4d78.038341?entry=ttu";
+    let display = document.getElementById("searchdisplay");
+    display.style.display = "";
+    display.innerHTML = "<h1>THIS IS CSE AND ECE BUILDINGS</h1>";
+    let anchor = document.getElementById("asearch");
+    anchor.style.display = "";
+    anchor.innerHTML = "<H3>CLICK HERE FOR DIRECTIONS</H3>"
+    anchor.href = "https://www.google.com/maps/place/15%C2%B045'40.6%22N+78%C2%B002'18.0%22E/@15.761278,78.0376973,279m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d15.761278!4d78.038341?entry=ttu";
 })
+
 let mech = document.getElementsByClassName('mech')[0];
-mech.addEventListener('click', ()=>{
+mech.addEventListener('click', () => {
     console.log("Clicked")
     let map = document.getElementsByClassName('map')[0];
     map.src = "maps/mech/mech.html";
-    let display =document.getElementById("searchdisplay");
-    display.style.display="none";
-    let anchor =document.getElementById("asearch");
-    anchor.style.display="none";
-    let display =document.getElementById("searchdisplay");
-        display.style.display="";
-        display.innerHTML="<h1>THIS IS NEW-ACADEMIC BUILDING</h1>";
-        let anchor =document.getElementById("asearch");
-        anchor.style.display="";
-        anchor.innerHTML="<H3>CLICK HERE FOR DIRECTIONS</H3>"
-        anchor.href="https://www.google.com/maps/place/15%C2%B045'40.9%22N+78%C2%B002'20.9%22E/@15.761361,78.0384853,279m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d15.761361!4d78.039129?entry=ttu";
+    let display = document.getElementById("searchdisplay");
+    display.style.display = "";
+    display.innerHTML = "<h1>THIS IS MECHANICAL BUILDING</h1>";
+    let anchor = document.getElementById("asearch");
+    anchor.style.display = "";
+    anchor.innerHTML = "<H3>CLICK HERE FOR DIRECTIONS</H3>"
+    anchor.href = "https://www.google.com/maps/place/15%C2%B045'40.0%22N+78%C2%B002'22.3%22E/@15.7611086,78.0382375,422m/data=!3m2!1e3!4b1!4m10!1m5!3m4!2zMTXCsDQ1JzQwLjkiTiA3OMKwMDInMjAuOSJF!8m2!3d15.7613611!4d78.0391389!3m3!8m2!3d15.761106!4d78.039525?entry=ttu";
 })
 let srkh = document.getElementsByClassName('srkh')[0];
-srkh.addEventListener('click', ()=>{
+srkh.addEventListener('click', () => {
     console.log("Clicked")
     let map = document.getElementsByClassName('map')[0];
     map.src = "maps/srkh/srkh.html";
-    let display =document.getElementById("searchdisplay");
-    display.style.display="none";
-    let anchor =document.getElementById("asearch");
-    anchor.style.display="none";
+    let display = document.getElementById("searchdisplay");
+    display.style.display = "none";
+    let anchor = document.getElementById("asearch");
+    anchor.style.display = "none";
 })
 
 
 let delhi = document.getElementsByClassName('delhi')[0];
-delhi.addEventListener('click', ()=>{
+delhi.addEventListener('click', () => {
     console.log("Clicked")
     let map = document.getElementsByClassName('map')[0];
     map.src = "maps/delhi/delhi.html";
-let display =document.getElementById("searchdisplay");
-        display.style.display="";
-        display.innerHTML="<h1>THIS IS DELHI CHAAT WALA</h1>";
-        let anchor =document.getElementById("asearch");
-        anchor.style.display="None";
-        
+    let display = document.getElementById("searchdisplay");
+    display.style.display = "";
+    display.innerHTML = "<h1>THIS IS DELHI CHAAT WALA</h1>";
+    let anchor = document.getElementById("asearch");
+    anchor.style.display = "None";
+
 })
 
 
 let newacademic = document.getElementsByClassName('new-academic')[0];
-newacademic.addEventListener('click', ()=>{
+newacademic.addEventListener('click', () => {
     console.log("Clicked")
     let map = document.getElementsByClassName('map')[0];
     map.src = "maps/new-academic/new-academic.html";
-let display =document.getElementById("searchdisplay");
-        display.style.display="";
-        display.innerHTML="<h1>THIS IS NEW ACADEMICS BUILDING</h1>";
-        let anchor =document.getElementById("asearch");
-        anchor.style.display="";
-        anchor.innerHTML="<H3>CLICK HERE FOR DIRECTIONS</H3>"
-        anchor.href="https://www.google.com/maps/place/15%C2%B045'40.9%22N+78%C2%B002'20.9%22E/@15.761361,78.0384853,279m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d15.761361!4d78.039129?entry=ttu";
+    let display = document.getElementById("searchdisplay");
+    display.style.display = "";
+    display.innerHTML = "<h1>THIS IS NEW ACADEMICS BUILDING</h1>";
+    let anchor = document.getElementById("asearch");
+    anchor.style.display = "";
+    anchor.innerHTML = "<H3>CLICK HERE FOR DIRECTIONS</H3>"
+    anchor.href = "https://www.google.com/maps/place/15%C2%B045'40.9%22N+78%C2%B002'20.9%22E/@15.761361,78.0384853,279m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d15.761361!4d78.039129?entry=ttu";
 })
 
 
 let kalam = document.getElementsByClassName('kalam')[0];
-kalam.addEventListener('click', ()=>{
+kalam.addEventListener('click', () => {
     console.log("Clicked")
     let map = document.getElementsByClassName('map')[0];
     map.src = "maps/kalam/kalam.html";
-    let display =document.getElementById("searchdisplay");
-        display.style.display="";
-        display.innerHTML="<h1>THIS IS KALAM HOSTEL</h1>";
-        let anchor =document.getElementById("asearch");
-        anchor.style.display="";
-        anchor.innerHTML="<H3>CLICK HERE FOR DIRECTIONS</H3>"
-        anchor.href="https://www.google.com/maps/place/Kalam+Hall+of+Residence/@15.7564369,78.0385818,778m/data=!3m1!1e3!4m12!1m5!3m4!2zMTXCsDQ1JzQwLjkiTiA3OMKwMDInMjAuOSJF!8m2!3d15.7613611!4d78.0391389!3m5!1s0x3bb5dd3828b50ef5:0x74bc24aa5e5cbe57!8m2!3d15.7582382!4d78.0396143!16s%2Fg%2F11sds6413x?entry=ttu";
+    let display = document.getElementById("searchdisplay");
+    display.style.display = "";
+    display.innerHTML = "<h1>THIS IS KALAM HOSTEL</h1>";
+    let anchor = document.getElementById("asearch");
+    anchor.style.display = "";
+    anchor.innerHTML = "<H3>CLICK HERE FOR DIRECTIONS</H3>"
+    anchor.href = "https://www.google.com/maps/place/Kalam+Hall+of+Residence/@15.7564369,78.0385818,778m/data=!3m1!1e3!4m12!1m5!3m4!2zMTXCsDQ1JzQwLjkiTiA3OMKwMDInMjAuOSJF!8m2!3d15.7613611!4d78.0391389!3m5!1s0x3bb5dd3828b50ef5:0x74bc24aa5e5cbe57!8m2!3d15.7582382!4d78.0396143!16s%2Fg%2F11sds6413x?entry=ttu";
 })
 
 
 let oldacademic = document.getElementsByClassName('old-academic')[0];
-oldacademic.addEventListener('click', ()=>{
+oldacademic.addEventListener('click', () => {
     console.log("Clicked")
     let map = document.getElementsByClassName('map')[0];
-let display =document.getElementById("searchdisplay");
-        display.style.display="";
-        display.innerHTML="<h1>THIS IS OLD ACADEMIC BUILDING</h1>";
-        let anchor =document.getElementById("asearch");
-        anchor.style.display="";
-        anchor.innerHTML="<H3>CLICK HERE FOR DIRECTIONS</H3>"
-        anchor.href="https://www.google.com/maps/place/Indian+Institute+Of+Information+Technology,+Design+%26+Manufacturing,+Kurnool/@15.756247,78.0434222,2734m/data=!3m1!1e3!4m6!3m5!1s0x3bb5dc3bfcf99645:0x52358ddcfb659cb9!8m2!3d15.7617232!4d78.0363767!16s%2Fg%2F11b8cdwgnh?entry=ttu";
+    let display = document.getElementById("searchdisplay");
+    display.style.display = "";
+    display.innerHTML = "<h1>THIS IS OLD ACADEMIC BUILDING</h1>";
+    let anchor = document.getElementById("asearch");
+    anchor.style.display = "";
+    anchor.innerHTML = "<H3>CLICK HERE FOR DIRECTIONS</H3>"
+    anchor.href = "https://www.google.com/maps/place/Indian+Institute+Of+Information+Technology,+Design+%26+Manufacturing,+Kurnool/@15.756247,78.0434222,2734m/data=!3m1!1e3!4m6!3m5!1s0x3bb5dc3bfcf99645:0x52358ddcfb659cb9!8m2!3d15.7617232!4d78.0363767!16s%2Fg%2F11b8cdwgnh?entry=ttu";
 })
 
 
 let mvhr = document.getElementsByClassName('mvhr')[0];
-mvhr.addEventListener('click', ()=>{
+mvhr.addEventListener('click', () => {
     console.log("Clicked")
     let map = document.getElementsByClassName('map')[0];
     map.src = "maps/mvhr/mvhr.html";
-let display =document.getElementById("searchdisplay");
-        display.style.display="";
-        display.innerHTML="<h1>THIS IS MVHR HOSTEL</h1>";
-        let anchor =document.getElementById("asearch");
-        anchor.style.display="";
-        anchor.innerHTML="<H3>CLICK HERE FOR DIRECTIONS</H3>"
-        anchor.href="https://www.google.com/maps/place/15%C2%B045'29.6%22N+78%C2%B002'25.6%22E/@15.758226,78.0392158,558m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d15.758226!4d78.040443?entry=ttu";
+    let display = document.getElementById("searchdisplay");
+    display.style.display = "";
+    display.innerHTML = "<h1>THIS IS MVHR HOSTEL</h1>";
+    let anchor = document.getElementById("asearch");
+    anchor.style.display = "";
+    anchor.innerHTML = "<H3>CLICK HERE FOR DIRECTIONS</H3>"
+    anchor.href = "https://www.google.com/maps/place/15%C2%B045'29.6%22N+78%C2%B002'25.6%22E/@15.758226,78.0392158,558m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d15.758226!4d78.040443?entry=ttu";
 })
 
 
 let kchor = document.getElementsByClassName('kchor')[0];
-kchor.addEventListener('click', ()=>{
+kchor.addEventListener('click', () => {
     console.log("Clicked")
     let map = document.getElementsByClassName('map')[0];
-let display =document.getElementById("searchdisplay");
-        display.style.display="";
-        display.innerHTML="<h1>THIS IS KALPANA CHAWLA GIRLS HOSTEL</h1>";
-        let anchor =document.getElementById("asearch");
-        anchor.style.display="";
-        anchor.innerHTML="<H3>CLICK HERE FOR DIRECTIONS</H3>"
-        anchor.href="https://www.google.com/maps/place/15%C2%B045'27.0%22N+78%C2%B002'18.9%22E/@15.7575,78.0385833,817m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d15.7575!4d78.0385833?entry=ttu";
+    let display = document.getElementById("searchdisplay");
+    display.style.display = "";
+    display.innerHTML = "<h1>THIS IS KALPANA CHAWLA GIRLS HOSTEL</h1>";
+    let anchor = document.getElementById("asearch");
+    anchor.style.display = "";
+    anchor.innerHTML = "<H3>CLICK HERE FOR DIRECTIONS</H3>"
+    anchor.href = "https://www.google.com/maps/place/15%C2%B045'27.0%22N+78%C2%B002'18.9%22E/@15.7575,78.0385833,817m/data=!3m2!1e3!4b1!4m4!3m3!8m2!3d15.7575!4d78.0385833?entry=ttu";
 })
 
 let c = 45;
 
-function draw(){
-  document.documentElement.style.setProperty('--direction', c++ + 'deg');
-  requestAnimationFrame(draw);
+function draw() {
+    document.documentElement.style.setProperty('--direction', c++ + 'deg');
+    requestAnimationFrame(draw);
 }
 
 requestAnimationFrame(draw);
